f5d3c7f89a9b4ba4ebbd04b6a11b62b0de3825ed
Evgeniy <evgen19-97@ya.ru>
2018-11-27 12:12:18 +0300

    change logic

--- С++/Khomutov.cpp
+++ С++/Khomutov.cpp
@@ -15,13 +15,22 @@ const double center[2] = {-0.7,0};
 const int iterations = 767; // 767 to large
 const double colour[3] = {0.2,0.6,1};
 
+//auto give_color(const int n)
+//{
+//    if (n < 256) return {unsigned(n),0,0};
+//    if (n < 512) return {255,unsigned(n - 255),0};
+//    return {255,255,unsigned(n - 511)};
+//}
+
 int main(int argc, char* argv[]){
     std::ios_base::sync_with_stdio(false);
     mkdir("result");
-    // std::vector <std::vector<double>> pic;
+    // create 3d massiv of unsigned char
+    unsigned char picture[screensize[0]][screensize[1]][3];
+
     for (int y = 0; y < screensize[1]; y++) 
     {
-        std::vector <double> row;
+        unsigned char row[screensize[0]][3];
         for (int x = 0; x < screensize[0];x++)
         {
             double constant[2] = {(x-screensize[0]/2)/scale+center[0],
@@ -29,13 +38,12 @@ int main(int argc, char* argv[]){
 
             double pos_[2] = {0.,0.} ;
             double pos_new[2] = {0.,0.};
-            double intensity = 0.;
+            int n = 0;
 
-            for (int n = 0; n < iterations; n++ )
+            for (n; n < iterations; n++ )
             {
                 if (pos_[0]*pos_[0]+pos_[1]*pos_[1] > 4) 
                 {
-                    intensity = 1000./n;
                     break;
                 }
 
@@ -46,25 +54,14 @@ int main(int argc, char* argv[]){
                 pos_[1] = pos_new[1];
             }
 
-            if (intensity > 255){
-                intensity = 255;
-            }
-            row.push_back(intensity);
+            // Вот тут понять
+            //row[x] = {255,255,unsigned(n - 511)};
+            //if (n < 512) row[x] = {255,unsigned(n - 255),0};
+            //if (n < 256) row[x] =  {unsigned(n),0,0};
+
         }
-
-        std::string path = "result/" + std::to_string(y) + ".txt";
-		std::ofstream out(path, std::ios_base::trunc);
-
-        for (auto i = row.begin(); i != row.end(); ++i)
-        {
-            out << *i << ' ';
-        }
-
-        out.close();
-        // pic.push_back(row);
+        // Тут добавление в picture row в заданное место
     }
-     // Запускаем построение картинки на Python
-    std::string command = "python Mandelbrot.py";
-    std::system(command.c_str());
+    // Тут должно быть создание картинки
     return 0;
 }
